97323815057c9b3869d775377c1a15c196d84b14
Jasdeep Singh <jasdeep.singh@qualitestgroup.com>
2022-07-27 11:19:03 +0530

    to main branch

--- src/test/java/com/herokuapp/theinternet/NewTest2.java
+++ src/test/java/com/herokuapp/theinternet/NewTest2.java
@@ -1,18 +1,23 @@
 package com.herokuapp.theinternet;
+
 import org.testng.annotations.Test;
+
 public class NewTest2 {
-  @Test (dependsOnMethods = { "OpenBrowser" })
-  public void SignIn() {
-	  System.out.println("User has signed in successfully");
-  }
+	@Test(dependsOnMethods = { "OpenBrowser" })
+	public void SignIn() {
+		System.out.println("User has signed in successfully");
+	}
 
-  @Test
-  public void OpenBrowser() {
-	  System.out.println("The browser is opened");
-  }
+	@Test
+	public void OpenBrowser() {
+		System.out.println("The browser is opened");
+		System.out.println("The user logged out successfully");
+		System.out.println("The user logged out successfully");
+		System.out.println("The user logged out successfully");
+	}
 
-  @Test (dependsOnMethods = { "SignIn" })
-  public void LogOut() {
-	  System.out.println("The user logged out successfully");
-  }
+	@Test(dependsOnMethods = { "SignIn" })
+	public void LogOut() {
+		System.out.println("The user logged out successfully");
+	}
 }
