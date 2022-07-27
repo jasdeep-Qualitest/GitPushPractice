2090e0de6b0ea469169207a922825eb14ae15133
Jasdeep Singh <jasdeep.singh@qualitestgroup.com>
2022-07-27 11:48:27 +0530

    to jasdeep practice branch

--- src/test/java/com/herokuapp/theinternet/NewTestNG.java
+++ src/test/java/com/herokuapp/theinternet/NewTestNG.java
@@ -33,7 +33,27 @@ public class NewTestNG {
 	        logMessage(expectedURL);
 	        
 }
-  private void logMessage(String expectedURL) {
+	public void ff() throws InterruptedException {
+		  
+	    String baseUrl = "https://www.toolsqa.com/";
+	    
+	      
+	    System.setProperty("webdriver.chrome.driver", "C:\\Users\\jasdeep.singh\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	        System.out.println("Launching Google Chrome browser"); 
+	        driver = new ChromeDriver();
+	       
+	        driver.get(baseUrl);
+	        String testTitle = "Just a moment...";
+	        Thread.sleep(5000);
+	        String originalTitle = driver.getTitle();
+	        Assert.assertEquals(originalTitle, testTitle);
+	        String expectedURL=driver.getCurrentUrl();
+	        Assert.assertEquals(baseUrl,expectedURL,expectedURL);
+	        logMessage(expectedURL);
+	        
+}
+		
+	private void logMessage(String expectedURL) {
 		// TODO Auto-generated method stub\
 	  System.out.println(expectedURL);
 		
